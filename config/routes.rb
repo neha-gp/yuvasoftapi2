60fa9494d8767f199a31961829fc862801b75e23
neha-gp <snehagurav0192@gmail.com>
2022-05-08 19:46:21 +0530

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,44 @@
+Rails.application.routes.draw do
+
+  namespace 'api' do
+    namespace 'v1' do
+      resources :categories
+    end
+  end
+
+  namespace 'api' do
+    namespace 'v1' do
+      resources :clientstetimonials
+    end
+  end
+
+  namespace 'api' do
+    namespace 'v1' do
+      resources :blogs
+    end
+  end
+
+  namespace 'api' do
+    namespace 'v1' do
+      resources :careers
+    end
+  end
+
+  namespace 'api' do
+    namespace 'v1' do
+      resources :company_events
+    end
+  end
+
+  namespace 'api' do
+    namespace 'v1' do
+      resources :teams
+    end
+  end
+
+  namespace 'api' do
+    namespace 'v1' do
+      resources :designations
+    end
+  end
+end
